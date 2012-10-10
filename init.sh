e0b5360b862e141cac7d27a1682e13e4adc49528
Tim Garton <garton.tim@gmail.com>
2012-10-10 12:35:57 -0700

    remove command-t from init.sh

--- init.sh
+++ init.sh
@@ -6,14 +6,14 @@ cd "${DIR}"
 git submodule init
 git submodule update
 
-#build command-t
-echo 'Building command-t.  Make sure you are building it against the version of ruby vim was built with (vim --version)...'
-cd submodules/command-t/ruby/command-t
-make clean
-rm Makefile
-ruby extconf.rb
-make
-cd ../../../..
+##build command-t
+#echo 'Building command-t.  Make sure you are building it against the version of ruby vim was built with (vim --version)...'
+#cd submodules/command-t/ruby/command-t
+#make clean
+#rm Makefile
+#ruby extconf.rb
+#make
+#cd ../../../..
 
 #check for needed commands
 command -v coffee >/dev/null 2>&1 || echo >&2 'coffee is not installed'
