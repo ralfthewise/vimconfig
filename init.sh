3d1ca3582c0cb07028f77bfa88b759da6b477929
Tim Garton <garton.tim@gmail.com>
2012-10-10 12:33:19 -0700

    instructions at end of init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+
 git submodule init
 git submodule update
 
@@ -15,3 +18,11 @@ cd ../../../..
 #check for needed commands
 command -v coffee >/dev/null 2>&1 || echo >&2 'coffee is not installed'
 command -v coffeelint >/dev/null 2>&1 || echo >&2 'coffeelint is not installed'
+
+echo ""
+echo ""
+echo ""
+echo "Installation complete."
+echo "  You should now do:"
+echo "    \"mv ~/.vimrc ~/.vimrc.bak; ln -s '${DIR}/vimrc' ~/.vimrc\""
+echo "    \"mv ~/.ctags ~/.ctags.bak; ln -s '${DIR}/ctags' ~/.ctags\""
