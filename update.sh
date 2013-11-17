4b8fd8f7dd758251de1d72d577fc910f86de0650
Tim Garton <garton.tim@gmail.com>
2013-11-17 12:26:33 -0800

    fix install/update

--- update.sh
+++ update.sh
@@ -1,51 +1,6 @@
 #!/bin/bash
 
-cd submodules/vim-pathogen
-git checkout master
-git pull
-cd ../..
-
 cd submodules/AutoComplPop
-git checkout master
-git pull
-cd ../..
-
-cd submodules/vim-bufexplorer
-git checkout master
-git pull
-cd ../..
-
-cd submodules/nerdtree
-git checkout master
-git pull
-cd ../..
-
-cd submodules/genutils
-git checkout master
-git pull
-cd ../..
-
-cd submodules/vim-rails
-git checkout master
-git pull
-cd ../..
-
-cd submodules/syntastic
-git checkout master
-git pull
-cd ../..
-
-cd submodules/vim-coffee-script
-git checkout master
-git pull
-cd ../..
-
-cd submodules/command-t
-git checkout master
-git pull
-cd ../..
-
-cd submodules/taglist
 git checkout master
 git pull
 cd ../..
@@ -55,10 +10,60 @@ git checkout master
 git pull
 cd ../..
 
+cd submodules/genutils
+git checkout master
+git pull
+cd ../..
+
+cd submodules/nerdcommenter
+git checkout master
+git pull
+cd ../..
+
+cd submodules/nerdtree
+git checkout master
+git pull
+cd ../..
+
+cd submodules/syntastic
+git checkout master
+git pull
+cd ../..
+
+cd submodules/taglist
+git checkout master
+git pull
+cd ../..
+
+cd submodules/vim-bufexplorer
+git checkout master
+git pull
+cd ../..
+
+cd submodules/vim-coffee-script
+git checkout master
+git pull
+cd ../..
+
+cd submodules/vim-pathogen
+git checkout master
+git pull
+cd ../..
+
+cd submodules/vim-rails
+git checkout master
+git pull
+cd ../..
+
+#cd submodules/command-t
+#git checkout master
+#git pull
+#cd ../..
+
 #build command-t
-cd submodules/command-t/ruby/command-t
-make clean
-rm -f Makefile
-ruby extconf.rb
-make
-cd ../../../..
+#cd submodules/command-t/ruby/command-t
+#make clean
+#rm -f Makefile
+#ruby extconf.rb
+#make
+#cd ../../../..
