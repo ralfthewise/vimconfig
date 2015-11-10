e3634de1b7ccee366ca3965f0047568e10843441
Tim Garton <garton.tim@gmail.com>
2015-11-10 11:40:22 -0800

    improve keyword completer

--- bundle/juggler/autoload/juggler/keyword_completer.rb
+++ bundle/juggler/autoload/juggler/keyword_completer.rb
@@ -11,8 +11,8 @@ module Juggler
       return if base.nil? || base.empty?
 
       file = nil
-      base_regex = Regexp.new(generate_keyword_pattern(base), Regexp::IGNORECASE)
-      keyword_output = VIM::evaluate("s:GetKeywords('/\\c#{Juggler.escape_vim_singlequote_string(generate_keyword_pattern(base))}')")
+      base_regex = Regexp.new(generate_keyword_match_pattern(base), Regexp::IGNORECASE)
+      keyword_output = VIM::evaluate("s:GetKeywords('#{Juggler.escape_vim_singlequote_string(generate_keyword_search_pattern(base))}')")
       keyword_output.split("\n").each do |line|
         if match = @@keyword_regexp.match(line)
           index = match[1].to_i
@@ -22,13 +22,18 @@ module Juggler
             yield(entry)
           end
         else
-          file = File.absolute_path(line)
+          file = File.expand_path(line)
         end
       end
     end
 
     protected
-    def generate_keyword_pattern(base)
+    def generate_keyword_search_pattern(base)
+      #'\c' makes it case insensitive
+      return '/\\c' + base.scan(/./).join('\w*')
+    end
+
+    def generate_keyword_match_pattern(base)
       return '\w*' + base.scan(/./).join('\w*') + '\w*'
     end
   end
